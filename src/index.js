4b5abc25935bdc13a59122680d8003590ac6448a
Marina <for-marina@mail.ru>
2021-03-12 13:06:34 +0100

    feat: implement towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let res = [];
+  
+  if(matrix !== undefined && matrix.length !== 0)
+  {
+    let str = '';
+    for(let i = 0; i < matrix.length; i++)
+      {
+        let tempstr = matrix[i].join();
+        if(i !== 0) str += ',';
+        str+= ((i+1)%2 === 0 )? tempstr.split(',').reverse().join(",") : tempstr;
+      }
+    res = str.split(',');
+  }
+  return res;
 }
